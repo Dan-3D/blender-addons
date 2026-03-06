c6cc79c1bc84a24d464baee4252a80e0a2a4a240
Dan-3D <dane@canva.com>
2026-03-06 18:07:29 +0100

    Update __init__.py

--- addons/CollectionToGLB_Dan/CollectionToGLB_Dan/__init__.py
+++ addons/CollectionToGLB_Dan/CollectionToGLB_Dan/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "description": "Export collections as GLB with automatic scaling and transforms",
     "category": "Import-Export",
 }
- 
+
 import bpy
 import bmesh
 import os
@@ -2518,6 +2518,7 @@ def unregister():
 
 if __name__ == "__main__":
     register()
+
 
 
 
