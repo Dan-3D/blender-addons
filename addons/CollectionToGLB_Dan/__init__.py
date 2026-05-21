6c1022cbccb1a39fb4262436b1442cc43a2cda64
Dan-3D <dane@canva.com>
2026-05-21 20:24:46 +0200

    Update __init__.py

--- addons/CollectionToGLB_Dan/__init__.py
+++ addons/CollectionToGLB_Dan/__init__.py
@@ -28,7 +28,7 @@ from bpy.props import BoolProperty
 GITHUB_USER = "Dan-3D"
 GITHUB_REPO = "blender-addons"
 ADDON_FOLDER = "CollectionToGLB_Dan"
-CURRENT_VERSION = (1, 0, 0)
+CURRENT_VERSION = (1, 1, 0)
 
 update_available = False
 latest_release_data = None
